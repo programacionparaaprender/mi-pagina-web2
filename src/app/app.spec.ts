e38e32dd31a4856acef8e0e1f79a3fd0393c60ac
programacionparaaprender <yancel209@gmail.com>
2025-11-25 13:16:40 -0500

    actualizando nombre del proyecto

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mf-host-auth0');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mi-pagina-web2');
   });
 });
--- src/docs/js/menu-wc.js
+++ src/docs/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">mf-host-auth0 documentation</a>
+                    <a href="index.html" data-type="index-link">mi-pagina-web2 documentation</a>
                 </li>
 
                 <li class="divider"></li>
